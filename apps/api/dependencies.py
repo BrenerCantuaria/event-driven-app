ee503efaf6da1a39e7119e3e4ad41bfcca06c39e
Brener Cantuaria <brenercantuariasantos@gmail.com>
2025-09-22 12:42:00 -0300

    feat: adiciona syspath para reconhecer a pasta core

--- apps/api/dependencies.py
+++ apps/api/dependencies.py
@@ -1,3 +1,11 @@
+import sys
+from pathlib import Path
+
+# Adiciona o diretório raiz do projeto ao Python path
+root_dir = Path(__file__).parent.parent.parent
+sys.path.insert(0, str(root_dir))
+
+
 from faststream.rabbit import RabbitBroker
 from core.config import settings
 
