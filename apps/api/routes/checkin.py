e3374de3375e3f99e9634e53e21e7bca5322664f
Brener Cantuaria <brenercantuariasantos@gmail.com>
2025-09-23 10:51:01 -0300

    feat: Adiciona injeção de dependência do publisher_broker

--- apps/api/routes/checkin.py
+++ apps/api/routes/checkin.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 from apps.api.models.checkin import VehicleCheckInData, ProcessingApiResponse
+from apps.api.dependencies import publisher_broker
+
 
 router = APIRouter()
+
+# Dependência para injetar o broker
+async def get_publisher():
+    return publisher_broker
 
 @router.post('/submeterCheckin', response_model=ProcessingApiResponse)
 async def submeter_checkin(data: VehicleCheckInData):
