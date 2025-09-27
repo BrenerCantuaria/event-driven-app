352b0677e1a48d615af2c591a1dea0fe10d83074
Brener Cantuaria <brenercantuariasantos@gmail.com>
2025-09-27 18:02:26 -0300

    feat: adiciona spot para representar uma vaga de estacionamento disponível ou reservada

---
+++ apps/api/models/vagas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field, constr
+from typing import Optional, List
+from uuid import UUID
+
+
+# -------------------------------
+# 1. Representação de uma vaga individual
+# -------------------------------
+
+class Spot(BaseModel):
+    """
+    Representa uma vaga de estacionamento disponível ou reservada
+    """
+
+    spotId: str = Field(..., description="Identificador único da vaga")
+    level: Optional[str] = Field(
+        None, description="Nível/andar onde a vaga está localizada"
+    )
+    position: Optional[str] = Field(None, description="Posições dentro do nível")
+    isAvailable: bool = Field(
+        None, description="Indica se a vaga está disponível para uso"
+    )
+    reservedUntil: Optional[str] = Field(
+        ...,
+        description="Data/hora limite para uso da vaga qunado esiver reservada (ISO 8610)",
+    )
+
